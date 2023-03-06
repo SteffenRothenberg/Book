d194dc5e9c7dc7d08c96acb9b7a24a36ede5e989
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2023-03-06 10:38:04 +0100

    git-ignore idea & target

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+            Student student1 = new Student();
+            student1.age = 23;
+            student1.studentNumber = "ABC123456";
+            student1.name="Steffen";
+            student1.surname="Rothenberg";
+
+
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,6 @@
+public class Student {
+    String name;
+    String surname;
+    int age;
+    String studentNumber;
+}
